867d14ac4c2d3aec0cd97f0eafbb798ab22fe782
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-06-19 17:09:34 +0200

    v2

--- prog.py
+++ prog.py
@@ -14,87 +14,133 @@ Height = list[6]
 
 class Actor(AbstractActor):
     peers: dict
-    peer_heights: dict
-    height: Height
-    
-    
+    peer_heights: dict#{int: dict(int, list[6])}
+    peer_pids: dict#dict(int,int)
+    heights: dict#dict(int, Height)
+    pid: int
     
     def __str__(self):
         return self.__repr__()
     def __repr__(self):
-        return "Actor {" + f"height={self.height}, peers={list(self.peer_heights.keys())}" + "}" 
+        return "Actor {" + f"height={self.heights}, peers={list(self.peer_heights.keys())}" + "}" 
     
     def __init__(self):
         super().__init__()
         self.peer_heights = {}
-        self.height = []
-        
-        
-    def suicide(self):
-        for p in self.peers.values():
-            p.tell({"type": "suicide", "id": self.height[-1]})
-        self.stop()
+        self.peer_pids = {}
+        self.heights = {}
         
     
     def getId(self):
-        return self.height[-1]
+        return self.heights[self.pid][-1]
     
     def broadcastUpdate(self):
-        print(f"{self.height[-1]} bc to {list(self.peers.keys())}")
+        print(f"{self.heights[self.pid][-1]} bc to {list(self.peers.keys())}")
         for peer in self.peers.values():
-            peer.tell({'type': 'update', 'height': self.height})
+            peer.tell({
+                'type': 'update', 
+                'heights': self.heights,
+                'id': self.heights[self.pid][-1],
+                'pid': self.pid
+            })
     
     def is_sink(self):
-        for peer in self.peer_heights.values():
-            if peer < self.height:
+        pid = self.pid
+        
+        for id in self.peer_heights.keys():
+            if self.peer_pids[id] != pid:
+                continue
+            
+            if self.peer_heights[id][pid] < self.heights[pid]:
                 return False
         return True
     
-    
+    # ref levels of peers in our partition
     def peer_ref_levels(self):
-        return [p[1:4] for p in self.peer_heights.values()]
-    
+        rl = []
+        
+        pid = self.pid
+        
+        for i in self.peers.keys():
+            if self.peer_pids == self.pid:
+                rl.append(self.peer_heights[i][pid][1:4])
+        return rl    
 
             
-    def update_message(self, height):
+    def update_message(self, id, heights, pid):
         sink = self.is_sink()
         
-        self.peer_heights[height[-1]] = height
-        print("update", self.height, self.peer_heights)
+        heights = heights.copy()
         
+        self.peer_heights[id] = heights
+        self.peer_pids[id] = pid
         
-        if height[0] != self.height[0]:
+        #print("update", self.heights, heights)
+        
+        need_broadcast = False
+        
+        if pid != self.pid:
+            # we know someone from another partition,
+            # they might know about other partitions
+            updated = False
+            for p in heights.keys():
+                h = heights[p]
+                if isinstance(h, list):
+                    h = 0
+                    
+                if p == self.pid: 
+                    continue
+                    
+                if p in self.heights.keys() and self.heights[pid] <= h + 1:
+                    continue
+                
+                updated = True
+                print(self.heights[self.pid][-1], "learn about", p)
+                self.heights[p] = h + 1
+                    
+                
+            if updated:
+                self.broadcastUpdate()
+            return
+        
+        else:
+            # pj knows some partitions, let us learn how to contact them
+            for p in heights.keys():
+                if p != self.pid:
+                    if not p in self.heights.keys() or self.heights[p] > heights[p] + 1:
+                        self.heights[p] = heights[p] + 1
+                        need_broadcast = True
+
+        if heights[pid][0] != self.heights[pid][0]:
             # When node i receives an Update message 
             # from neighboring node j such that lidj != lidi: 
             
         
             # if lidi > lidj or (oidi = lidj and ri = 1)
-            if self.height[0] > height[0] or (self.height[2] == height[2] and self.height[2] == 1):
-                self.height = [height[0], 0, 0, 0, height[4]+1, self.height[-1]]
-                print(f"adopt {self.height[-1]} -> {height[-1]}")
+            if (self.heights[pid][0] > heights[pid][0] or 
+               (self.heights[pid][2] == heights[pid][2] and self.heights[pid][2] == 1)):
+                self.heights[pid] = [heights[pid][0], 0, 0, 0, heights[pid][4]+1, self.heights[pid][-1]]
+                print(f"adopt {self.heights[pid][-1]} -> {heights[pid][-1]}")
                 
                 assert not self.is_sink()
                 
-                self.broadcastUpdate()
+                need_broadcast = True
                 
-
-        
-        # print("sink", sink, " ---> ", self.is_sink())
-        
-        if sink:
-            return
-        
-        print("becomes sink", self.height)
-        
-        
-        h = self.height  
                 
-        if self.update_height(height[-1]):
-            print("updated height", self.height)
+            if not sink:
+                print("becomes sink", self.heights[self.pid])
+                
+                
+                h = self.heights[pid]  
+                        
+                if self.update_height(heights[pid][-1]):
+                    print("updated height", self.heights[pid])
+                    need_broadcast = True
+                else:
+                    assert h == self.heights[self.pid]
+        
+        if need_broadcast:
             self.broadcastUpdate()
-        else:
-            assert h == self.height
-        
              
     def suicide_message(self, id):
         del self.peers[id]
@@ -103,17 +149,17 @@ class Actor(AbstractActor):
         if self.is_sink():
             if self.peers == {}:
                 # leader of its own group
-                id = self.height[-1]
-                self.height = [id, -1, -1, -1, 0, id]
+                id = self.heights[self.pid][-1]
+                self.heights[self.pid] = [id, -1, -1, -1, 0, id]
             else:
-                print("NEW SINK", self.height)
-                id = self.height[-1]
+                print("NEW SINK", self.heights[self.pid])
+                id = self.heights[self.pid][-1]
                 t = time.time()
-                self.height[1:4] = [t, id, 0]
+                self.heights[self.pid][1:4] = [t, id, 0]
                 
             self.broadcastUpdate()
         else:
-        
+            pass
         
         
         
@@ -129,7 +175,7 @@ class Actor(AbstractActor):
         if not self.is_sink():
             return False
         
-        print("SINK", self.height, self.peer_heights)
+        print("SINK", self.heights[self.pid], self.peer_heights)
         
         
         rl = self.peer_ref_levels()
@@ -143,21 +189,19 @@ class Actor(AbstractActor):
             
             # B
             
-            print("B")
             
             ref_level = max(self.peer_ref_levels())
             
             # partial link reversal
-            delta = self.height[4]
+            delta = self.heights[self.pid][4]
             for p in self.peer_heights.values():
                 if p[1:4] == ref_level:
                     delta = min(delta, p[4] - 1)
             
-            self.height[1:4] = ref_level
-            self.height[4] = delta
+            self.heights[self.pid][1:4] = ref_level
+            self.heights[self.pid][4] = delta
             
         else:
-            print("ELSEEEEEEEEEE")
             peers_ref = rl[0]
             if peers_ref[2] == 0: # r == 0
                 # unreflected ref level
@@ -167,9 +211,8 @@ class Actor(AbstractActor):
                 # the reflected version of its neighbors' 
                 # (with ri = 1) and its delta to 0
                 
-                print("C - REFLECTIONNNNNNNNNNNNNNNNNNNN")
-                self.height[1:4] = [peers_ref[0], peers_ref[1], 1]
-                self.height[4] = 0
+                self.heights[self.pid][1:4] = [peers_ref[0], peers_ref[1], 1]
+                self.heights[self.pid][4] = 0
             elif peers_ref[1] == i: # r == 1, oid == i
                 # all of i's neighbors have the same reflected reference
                 # level with i as the originator, then i has detected a
@@ -177,8 +220,8 @@ class Actor(AbstractActor):
                 
                 # D
                 print("NEW LEADER ELECTED")
-                id = self.height[-1]
-                self.height = [id, -1, -1, -1, 0, id]
+                id = self.heights[self.pid][-1]
+                self.heights[self.pid] = [id, -1, -1, -1, 0, id]
             else:
                 #all of i's neighbors have the same reflected reference
                 #level with an originator other than i, then i starts a
@@ -190,9 +233,11 @@ class Actor(AbstractActor):
         return True
 
 
-    def init_message(self, actor_id, peers):
-        self.height = [actor_id, -1, -1, -1, 0, actor_id]
+    def init_message(self, actor_id, peers, pid):
+        self.heights = {pid: [actor_id, -1, -1, -1, 0, actor_id]}
         self.peers = peers
+        self.pid = pid
+        
     
     
     def start_message(self):
@@ -206,64 +251,71 @@ class Actor(AbstractActor):
         
 
 # return actor refs and links
-def init_actors(N, p):
-    actorrefs = {i: Actor().start() for i in range(N)}
+def init_actors(N, p, P, k):
+    actorrefs = {i: Actor().start() for i in range(N * k)}
     
-    links = {i: [] for i in range(N)}
+    links = {i: [] for i in range(N * k)}
+    
+    np.random.seed(0)
     
     
-    # np.random.seed(0)
-    
-    for i in range(N):
-        others = [j for j in range(N)]
-        others.remove(i)
+    # partitions
+    for j in range(k):    
+        for i in range(N):
+            begin = N * j
+            others = [l for l in range(begin, begin + N)]
+            others.remove(begin + i)
+            
+            
+            added_links = list(np.random.choice(others, p, replace=False))
+            
+            links[begin + i] += added_links
+            for o in added_links:
+                links[o].append(begin + i)
+                
+    # links between partitions
+    if k > 1:
+        for pi in range(k):
+            part = [i for i in range(N * pi, N * (pi + 1))]
+            
+            others = [l for l in range(k * N) if l not in part]
+            print(others, k, N)
+            
+            for i in range(P):
+                host = np.random.choice(part)
+                foreigner = np.random.choice(others)
+                
+                links[host]      += [foreigner]
+                links[foreigner] += [host]
         
-        
-        added_links = list(np.random.choice(others, p, replace=False))
-        
-        links[i] += added_links
-        for j in added_links:
-            links[j].append(i)
+            
     
     return actorrefs, links
 
+# N: nodes per partition
+# p: conectivity parameter in a partition
+# P: conectivity parameter between partitions
+# k: number of partitions
+N = 6; p = 2; P = 2; k = 3
 
-N = 20; p = 3
+actor_refs, links = init_actors(N, p, P, k)
 
-actor_refs, links = init_actors(N, p)
 
 actors = [a.ask({"type": "getself"}) for a in actor_refs.values()]
-
-# pring average of number of links per actor
-#print(np.mean([len(links[i]) for i in range(N)]))
-
-# print variance of number of links per actor
-#print(np.var([len(links[i]) for i in range(N)]))
-
-
 
 
 
 # send set_id message to all actors
 for i in range(len(actors)):
+    pid = i // N
+    
     actor_refs[i].ask({
         "type": "init",
         "actor_id": i,
         "peers": {id: actor_refs[id] for id in links[i]},
+        "pid": pid
     })
 
-
-"""
-    kill the actor with id = i
-"""
-def kill_actor(i: int):
-    # find actor with height[-1] = i
-    a = [a for a in actors if a.height[-1] == i][0]
-
-    a.suicide()
-    actors.remove(a)
-    del actor_refs[i]
-    
 
 print(actors)
 
@@ -273,58 +325,128 @@ for a in actor_refs.values():
     })
 
 
-sleep(1)
+sleep(2)
 
 
-print(*actors, sep=",\n")
+for a in actor_refs.values():
+    a.stop()
 
 
+for i in range(len(actors)):
+    a = actors[i]
+    print(i, a)
 
 
+import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def draw_graph(actors, links):
-    import networkx as nx
     
     G = nx.DiGraph()
-    G.add_nodes_from(range(len(actors)))
+    partitions_colors = {
+        0: "yellow", 
+        1: "cyan",
+        2: "lightgreen",
+        3: "lightblue",
+        4: "red",
+        5: "pink",
+        6: "yellow",
+        6: "cyan",
+        7: "lightgreen",
+        8: "grey",
+        9: "red",
+    }
+    node_tuples = [0 for _ in range(N * k)]
+    
+    for (i, a) in enumerate(actors):
+        node_tuples[i] = (i, {'color': partitions_colors[a.pid]})
+    
+    G.add_nodes_from(node_tuples)
+    
 
     
     # add from link list
     for a in actors:
         # heights of neighbors
         
-        aid = a.height[-1]
+        aid = a.heights[a.pid][-1]
+        outgoing = []
+        
+        inter_part_links = []
+        inter_part_h0 = []
         
         
-        outgoing = [h for h in a.peer_heights.values() if h < a.height]
+        for i in a.peer_heights.keys():
+            pidi = a.peer_pids[i]
+            h = a.peer_heights[i][pidi]
+            
+            if 0 in a.peer_heights[i]:
+                if pidi == 0:
+                    inter_part_h0.append(0)
+                else:
+                    inter_part_h0.append(a.peer_heights[i][0])
+                
+                inter_part_links.append(h[-1])
+                
+            if pidi == a.pid:
+                if h < a.heights[a.pid]:
+                    outgoing.append(h)
+            else:
+                if aid > h[-1]:
+                    G.add_edge(aid, h[-1], color="blue", weight=2, style="arc3")
+                
+        
+        
+        if a.pid != 0 and inter_part_links != []:
+            link = inter_part_links[np.argmin(inter_part_h0)]
+            print("LINK", aid, link)
+            G.add_edge(aid, link, color="green", weight=2, style="arc3")
         
         if outgoing == []:
             continue
-            
+        
+        
+        
         min_link = min(outgoing)
         
         outgoing.remove(min_link)
         
-        G.add_edge(aid, min_link[-1], color="red", weight=2)
+        G.add_edge(aid, min_link[-1], color="red", weight=2, style="arc3")
 
         for o in outgoing:
             # grey edge
-            G.add_edge(aid, o[-1], color="grey", weight=1)
+            G.add_edge(aid, o[-1], color="grey", weight=1, style="arc3")
             
             
     # pos = nx.kamada_kawai_layout(G)
 
     edges = G.edges()
-    colors = [G[u][v]['color'] for u,v in edges]
+    edge_colors = [G[u][v]['color'] for u,v in edges]
     weights = [G[u][v]['weight'] for u,v in edges]
+    connectionstyle = [G[u][v]['style'] for u,v in edges]
+    
+    node_colors = [G.nodes[i]['color'] for i in G.nodes()]
+    
+    
+    
+    pos = nx.kamada_kawai_layout(G)
 
-    nx.draw(G, edge_color=colors, width=weights, with_labels=True, node_size=800, node_color="black", font_color="white")
+    nx.draw(G,
+            pos=pos,
+            connectionstyle='arc3, rad=0.1',
+            edge_color=edge_colors, 
+            width=weights, 
+            with_labels=True, 
+            node_size=800, 
+            node_color=node_colors, 
+            font_color="black"
+    )
     
     
 draw_graph(actors, links)
 
-for a in actor_refs.values():
-    a.stop()
+    
+
 plt.show()
 
